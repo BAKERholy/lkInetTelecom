4e9b8b952ab760daa4298354d72023403e4b0dd6
Дмитрий <76389902+BAKERholy@users.noreply.github.com>
2023-01-12 20:45:16 +0300

    transition

--- libs/index.js
+++ libs/index.js
@@ -2,12 +2,13 @@ const navToggle = document.querySelector(".navigation__toggle");
 const navList = document.querySelector(".navigation-list");
 const nav = document.querySelector(".navigation");
 
-navToggle.classList.add("navigation__toggle--v");
-nav.classList.add("not-active");
 
 navToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
-  nav.classList.toggle("not-active");
-  
+  nav.classList.toggle("active");
+  navToggle.classList.toggle("active");
+
 });
+
+
 

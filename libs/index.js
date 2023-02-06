b399ba4c752addead64bacabc43a9a9652b32d86
Дмитрий <76389902+BAKERholy@users.noreply.github.com>
2023-02-06 10:34:36 +0300

    edits

--- libs/index.js
+++ libs/index.js
@@ -2,23 +2,17 @@ const navToggle = document.querySelector(".navigation__toggle");
 const navList = document.querySelector(".navigation-list");
 const nav = document.querySelector(".navigation");
 
-
+console.log(navToggle)
 navToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
   nav.classList.toggle("active");
   navToggle.classList.toggle("active");
-
 });
 
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 2000);
+// var slides = document.querySelectorAll('#slides .slide');
+// var currentSlide = 0;
+// var slideInterval = setInterval(nextSlide, 2000);
 
-function nextSlide() {
-  slides[currentSlide].className = 'slide';
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].className = 'slide showing';
-}
 
 
 

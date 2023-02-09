342814c39602e93e8de7dc39674dd27773e8accc
Alex Blagov <ablagov@rzdit.ru>
2023-02-09 22:24:55 +0300

    feat_detailing_page

--- libs/index.js
+++ libs/index.js
@@ -2,7 +2,23 @@ const navToggle = document.querySelector(".navigation__toggle");
 const navList = document.querySelector(".navigation-list");
 const nav = document.querySelector(".navigation");
 
-console.log(navToggle)
+const tabs = document.querySelectorAll(".detailing-links a");
+const tables = document.querySelectorAll(".wrapper-tabel");
+const detailingLinks = document.querySelector(".detailing-links");
+
+tabs.forEach((el, i) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    removeAllActiveClasses();
+    tables[i].classList.toggle("active");
+    detailingLinks.classList.add("not-active");
+  });
+});
+
+function removeAllActiveClasses() {
+  for (const tab of tables) tab.classList.remove("active");
+}
+
 navToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
   nav.classList.toggle("active");
@@ -12,12 +28,3 @@ navToggle.addEventListener("click", function (evt) {
 // var slides = document.querySelectorAll('#slides .slide');
 // var currentSlide = 0;
 // var slideInterval = setInterval(nextSlide, 2000);
-
-
-
-
-
-
-
-
-
